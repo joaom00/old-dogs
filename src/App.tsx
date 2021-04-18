95cf521ce82eca4c8166abea2362c3778351ac19
João Pedro <joaomljcanal@gmail.com>
2021-04-18 10:44:05 -0300

    Initial app config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+
 const App: React.FC = () => {
-  return <h1>App</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+
+      <GlobalStyle />
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    color: #333;
+    --type-first: Helvetica, Arial, sans-serif;
+    --type-second: 'Spectral', Georgia;
+    font-family: var(--type-first)
+  }
+
+  h1, h2, h3, h4, p {
+    margin: 0;
+  }
+
+  ul, li {
+    list-style: none;
+    padding: 0px;
+    margin: 0px;
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+  }
+
+  button, input {
+    display: block;
+    font-size: 1rem;
+    font-family: var(--type-first);
+    color: #333;
+  }
+`;
