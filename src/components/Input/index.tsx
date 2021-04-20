e9a0cd212273609e485981d730301417c2a14362
João Pedro <joaomljcanal@gmail.com>
2021-04-20 12:13:49 -0300

    useForm hook

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,37 @@
+import { ChangeEvent } from 'react';
 import * as S from './styles';
 
 interface InputProps {
   label: string;
   type: string;
   name: string;
+  value: string;
+  error: string;
+  onChange({ target }: ChangeEvent<HTMLInputElement>): void;
+  onBlur(): void;
 }
 
-const Input: React.FC<InputProps> = ({ label, type, name }) => {
+const Input: React.FC<InputProps> = ({
+  label,
+  type,
+  name,
+  value,
+  onChange,
+  error,
+  onBlur
+}) => {
   return (
     <S.Box>
       <S.Label htmlFor={name}>{label}</S.Label>
-      <S.Input id={name} name={name} type={type} />
-      <S.ErrorMessage>Error</S.ErrorMessage>
+      <S.Input
+        id={name}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <S.ErrorMessage>{error}</S.ErrorMessage>}
     </S.Box>
   );
 };
--- src/components/SignInForm/index.tsx
+++ src/components/SignInForm/index.tsx
@@ -1,35 +1,40 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent } from 'react';
 import { Link } from 'react-router-dom';
+import useForm from '../../hooks/useForm';
+import Button from '../Button';
+import Input from '../Input';
 
 import * as S from './styles';
 
 const SignInForm: React.FC = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const username = useForm();
+  const password = useForm();
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ username, password })
-    })
-      .then((response) => {
-        return response.json();
+
+    if (username.validate() && password.validate()) {
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        }
       })
-      .then((json) => {
-        console.log(json);
-      });
+        .then((response) => {
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
+    }
   };
 
   return (
     <S.Box>
       <S.Title>Login</S.Title>
       <S.Form onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
 
         <Button>Entrar</Button>
       </S.Form>
