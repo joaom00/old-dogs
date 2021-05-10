738e8cc116601ee23589688d65d964ab3017647c
João Pedro <joaomljcanal@gmail.com>
2021-05-10 20:36:54 -0300

    style: input component

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,7 +1,28 @@
 import styled from 'styled-components';
+import convertPixelToRem from '../../utils/convertPixelToRem';
 
-export const Box = styled.div``;
+export const Box = styled.div`
+  display: flex;
+  flex-direction: column;
 
-export const Label = styled.label``;
+  & + & {
+    margin-top: ${convertPixelToRem(32)};
+  }
+`;
 
-export const Input = styled.input``;
+export const Label = styled.label`
+  font-size: ${convertPixelToRem(16)};
+  color: #6e7191;
+  margin-bottom: ${convertPixelToRem(8)};
+`;
+
+export const Input = styled.input`
+  border: 1px solid #d9dbe9;
+  background: none;
+  padding: ${convertPixelToRem(16)} ${convertPixelToRem(24)};
+  border-radius: 8px;
+
+  &:focus {
+    border: 1px solid #ffbb12;
+  }
+`;
