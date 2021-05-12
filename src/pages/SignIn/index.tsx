b305ffe6e5776cf35326e3ef5ad17c4cd48a2911
João Pedro <joaomljcanal@gmail.com>
2021-05-12 20:12:59 -0300

    refactor: login page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,28 +1,75 @@
-import Input from '../../components/Input';
-import Button from '../../components/Button';
+import { FormEvent, useState } from 'react';
 
 import logoImg from '../../assets/dogs.svg';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import useAuth from '../../hooks/useAuth';
+
 import * as S from './styles';
+import { useHistory } from 'react-router';
 
 const SignInForm: React.FC = () => {
-  return (
-    <S.Box>
-      <img src={logoImg} alt="" />
-      <p>Dogs</p>
-      <S.Form>
-        <fieldset>
-          <S.Title>Iniciar sessão</S.Title>
-          <S.Description>
-            Já possui uma conta? Faça login aqui embaixo.
-          </S.Description>
-          <Input name="username" label="Username" />
-          <Input name="password" label="Senha" type="password" />
-        </fieldset>
+  const { signIn } = useAuth();
+  const history = useHistory();
 
-        <Button>Entrar</Button>
-      </S.Form>
-    </S.Box>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    await signIn({ email, password });
+
+    history.push('/');
+  }
+
+  return (
+    <S.Wrapper>
+      <S.SignInFormWrapper>
+        <S.Logo>
+          <img src={logoImg} alt="" />
+          <p>Dogs</p>
+        </S.Logo>
+        <S.SignInForm onSubmit={handleSubmit}>
+          <fieldset>
+            <S.Title>Iniciar sessão</S.Title>
+            <S.Description>
+              Já possui uma conta? Faça login aqui embaixo.
+            </S.Description>
+            <Input
+              type="email"
+              name="email"
+              label="E-mail"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <Input
+              type="password"
+              name="password"
+              label="Senha"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </fieldset>
+
+          <Button type="submit" mt={48}>
+            Entrar
+          </Button>
+
+          <S.LinksWrapper>
+            <p>
+              Não tem conta? <S.SignUpLink to="/signup">Crie uma</S.SignUpLink>
+            </p>
+            <S.ForgotPasswordLink to="/forgot-password">
+              Esqueceu sua senha?
+            </S.ForgotPasswordLink>
+          </S.LinksWrapper>
+        </S.SignInForm>
+      </S.SignInFormWrapper>
+      <S.Background />
+    </S.Wrapper>
   );
 };
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,8 @@
+import { Link } from 'react-router-dom';
 import styled, { keyframes } from 'styled-components';
+import convertPixelToRem from '../../utils/convertPixelToRem';
+
+import backgroundImg from '../../assets/login.jpg';
 
 const animeLeft = keyframes`
   to {
@@ -7,35 +11,126 @@ const animeLeft = keyframes`
   }
 `;
 
-export const Box = styled.section`
+export const Wrapper = styled.section`
+  height: 100vh;
+
   opacity: 0;
   transform: translateX(-20px);
   animation: ${animeLeft} 0.3s forwards;
+
+  display: flex;
 `;
 
-export const Form = styled.form`
-  margin-bottom: 2rem;
+export const SignInFormWrapper = styled.div`
+  padding: ${convertPixelToRem(64)} ${convertPixelToRem(32)}
+    ${convertPixelToRem(0)} ${convertPixelToRem(32)};
+
+  width: 100%;
+  max-width: ${convertPixelToRem(600)};
+
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 768px) and (max-width: 1024px) {
+    padding-left: ${convertPixelToRem(0)};
+    max-width: 100vw;
+    align-items: center;
+  }
+
+  @media (min-width: 1024px) {
+    padding-left: ${convertPixelToRem(0)};
+  }
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media (min-width: 768px) and (max-width: 1024px) {
+    align-self: flex-start;
+    padding-left: ${convertPixelToRem(165)};
+  }
+
+  @media (min-width: 1024px) {
+    margin-left: ${convertPixelToRem(80)};
+  }
+
+  p {
+    font-family: Spectral;
+    color: #14142b;
+    font-size: ${convertPixelToRem(36)};
+    margin-left: ${convertPixelToRem(16)};
+  }
+`;
+
+export const SignInForm = styled.form`
+  margin-top: ${convertPixelToRem(80)};
+
+  @media (min-width: 1024px) {
+    margin-left: ${convertPixelToRem(80)};
+  }
+
+  width: 100%;
+  max-width: ${convertPixelToRem(400)};
 `;
 
 export const Title = styled.legend`
   font-family: Spectral;
-  line-height: 1;
-  font-size: 3rem;
-  margin: 1rem 0;
+  color: #14142b;
+  font-size: ${convertPixelToRem(36)};
+  margin-bottom: ${convertPixelToRem(8)};
+  line-height: 46px;
+
   position: relative;
 
   &::after {
     content: '';
     display: block;
     position: absolute;
-    width: 1.5rem;
-    height: 1.5rem;
+    width: 1.2rem;
+    height: 1.2rem;
     background: #fb1;
-    bottom: 5px;
+    bottom: 8px;
     left: -5px;
     border-radius: 0.2rem;
     z-index: -1;
   }
 `;
 
-export const Description = styled.p``;
+export const Description = styled.p`
+  color: #a0a3bd;
+  font-style: normal;
+  font-weight: 500;
+  font-size: ${convertPixelToRem(16)};
+  line-height: 26px;
+  margin-bottom: ${convertPixelToRem(48)};
+`;
+
+export const LinksWrapper = styled.div`
+  text-align: center;
+  margin-top: ${convertPixelToRem(32)};
+
+  p {
+    color: #14142b;
+    font-size: ${convertPixelToRem(16)};
+    margin-bottom: ${convertPixelToRem(16)};
+  }
+`;
+
+export const SignUpLink = styled(Link)`
+  color: #ffbb12;
+  font-weight: 500;
+  font-size: ${convertPixelToRem(16)};
+`;
+
+export const ForgotPasswordLink = styled(Link)`
+  color: #ffbb12;
+  font-weight: 500;
+  font-size: ${convertPixelToRem(16)};
+`;
+
+export const Background = styled.div`
+  background: url(${backgroundImg}) no-repeat center;
+  background-size: cover;
+  flex: 1;
+`;
