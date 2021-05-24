23caa386294d905c6fc4899833db107eb087dccb
João Pedro <joaomljcanal@gmail.com>
2021-05-24 20:28:01 -0300

    Add Logo component

---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,56 @@
+import styled, { css, DefaultTheme } from 'styled-components';
+
+import { SignInFormWrapper } from '../../pages/SignIn/styles';
+
+import { LogoProps } from '.';
+
+const modifiers = {
+  black: (theme: DefaultTheme) => css`
+    color: ${theme.colors.gray.titleActive};
+  `,
+
+  yellow: (theme: DefaultTheme) => css`
+    color: ${theme.colors.primary[500]};
+  `,
+
+  normal: () => css`
+    height: 2.5rem;
+  `,
+
+  large: () => css`
+    height: 3.4rem;
+  `
+};
+
+export const Wrapper = styled.div<LogoProps>`
+  ${({ theme, color, size }) => css`
+    display: flex;
+    align-items: center;
+
+    svg {
+      height: 100%;
+    }
+
+    p {
+      font-family: ${theme.font.heading};
+      font-weight: ${theme.font.bold};
+      color: ${theme.colors.gray.titleActive};
+      font-size: ${theme.font.sizes.xlarge};
+      margin-left: ${theme.spacings.xsmall};
+    }
+
+    ${!!color && modifiers[color](theme)}
+    ${!!size && modifiers[size]}
+
+    ${SignInFormWrapper} & {
+      @media ${theme.media.between('medium', 'large')} {
+        align-self: flex-start;
+        padding-left: 16.5rem;
+      }
+
+      @media ${theme.media.greaterThan('large')} {
+        margin-left: 8rem;
+      }
+    }
+  `}
+`;
