8571cc954b7778c091db275abc1af77d1202c8de
João Pedro <joaomljcanal@gmail.com>
2021-06-04 23:56:45 -0300

    Add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
 import theme from './styles/theme';
 
 import AppProvider from './contexts';
@@ -8,16 +10,20 @@ import Routes from './routes';
 
 import GlobalStyles from './styles/global';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <AppProvider>
-          <Routes />
-        </AppProvider>
-      </Router>
-      <GlobalStyles />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <AppProvider>
+            <Routes />
+          </AppProvider>
+        </Router>
+        <GlobalStyles />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
