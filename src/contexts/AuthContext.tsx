763bb5b933c7945cdb0f2372095cf0d84849589d
João Pedro <joaomljcanal@gmail.com>
2021-06-14 21:40:42 -0300

    Refactor AuthContext

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState, useCallback } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import api from '../services/api';
 
 export type User = {
@@ -6,6 +6,7 @@ export type User = {
   name: string | null;
   username: string;
   email: string;
+  posts: number;
   followers: number;
   following: number;
   avatarUrl: string | undefined;
@@ -20,8 +21,8 @@ type SignInCredentials = {
   password: string;
 };
 
-export type AuthContextData = {
-  user: User;
+type AuthContextData = {
+  user: User | undefined;
   isAuthenticated: boolean;
   signIn: (credentials: SignInCredentials) => Promise<void>;
   signOut: () => void;
@@ -30,9 +31,25 @@ export type AuthContextData = {
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User>();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [user, setUser] = useState<User>({} as User);
+
+  useEffect(() => {
+    const token = localStorage.getItem('@Dogs::token');
+
+    if (token) {
+      api
+        .get('profile/me')
+        .then((response) => {
+          console.log(response);
+          setUser(response.data);
+          setIsAuthenticated(true);
+        })
+        .catch(() => {
+          signOut();
+        });
+    }
+  }, []);
 
   async function signIn({ emailOrUsername, password }: SignInCredentials) {
     const response = await api.post('sessions', {
@@ -44,45 +61,22 @@ const AuthProvider = ({ children }: AuthProviderProps) => {
 
     localStorage.setItem('@Dogs::token', token);
 
-    api.defaults.headers.authorization = `Bearer ${token}`;
+    api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
     setUser(user);
     setIsAuthenticated(true);
   }
 
-  const signOut = useCallback(() => {
+  function signOut() {
     localStorage.removeItem('@Dogs::token');
-    setUser({} as User);
     setIsAuthenticated(false);
-  }, []);
-
-  useEffect(() => {
-    async function getUser() {
-      const token = localStorage.getItem('@Dogs::token');
-      if (token) {
-        try {
-          setIsLoading(true);
-          api.defaults.headers.authorization = `Bearer ${token}`;
-          const { data } = await api.get('profile');
-          setUser(data);
-          setIsAuthenticated(true);
-        } catch {
-          signOut();
-          setIsAuthenticated(false);
-        } finally {
-          setIsLoading(false);
-        }
-      }
-    }
-
-    getUser();
-  }, [signOut]);
-
-  if (isLoading) {
-    return <p>Carregando...</p>;
   }
 
-  return <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export { AuthContext, AuthProvider };
--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,11 +1,14 @@
 import { AuthProvider } from './AuthContext';
+import { ModalProvider } from './ModalContext';
 
 type AppProviderProps = {
   children: React.ReactNode;
 };
 
 const AppProvider = ({ children }: AppProviderProps) => (
-  <AuthProvider>{children}</AuthProvider>
+  <AuthProvider>
+    <ModalProvider>{children}</ModalProvider>
+  </AuthProvider>
 );
 
 export default AppProvider;
