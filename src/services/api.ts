d4988f919aec080410104e5a4cfaf99cfd60d1a8
João Pedro <joaomljcanal@gmail.com>
2021-06-14 21:44:09 -0300

    Refactor api service to logout user automatic

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,24 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+
+const token = localStorage.getItem('@Dogs::token');
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
-})
+  baseURL: 'http://localhost:3333',
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      if (error.response.data.message === 'Invalid JWT token') {
+        localStorage.removeItem('@Dogs::token');
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
