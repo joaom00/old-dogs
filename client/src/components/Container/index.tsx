754bea4303dbfd1e3dcb02e314cf1f80790a0ef9
João Pedro <joaopsilvamagalhaes@gmail.com>
2021-09-30 22:29:46 -0300

    feat: add container component

---
+++ client/src/components/Container/index.tsx
@@ -0,0 +1,11 @@
+import styled, { css } from 'styled-components';
+
+const Container = styled.div`
+  ${({ theme }) => css`
+    max-width: ${theme.grid.container};
+    margin-left: auto;
+    margin-right: auto;
+  `}
+`;
+
+export default Container;
