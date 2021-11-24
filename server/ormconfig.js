79065b0951c7c3d186c17bd17a4e4d607e782d25
João Pedro <joaopsilvamagalhaes@gmail.com>
2021-11-24 12:23:19 -0300

    fix: downgrade class-transform package to version 0.4.0

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,6 +1,9 @@
 module.exports = {
   type: 'postgres',
-  ssl: false,
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
   host: process.env.TYPEORM_HOST,
   port: process.env.TYPEORM_PORT,
   username: process.env.TYPEORM_USERNAME,
